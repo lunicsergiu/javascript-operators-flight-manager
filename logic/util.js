463c40f904cc4676eb0331273eb9bb6b1b2b0548
lunicsergiu <lunicsergiu1999@gmail.com>
2022-04-11 10:49:42 +0200

    manage flights and passangers

--- logic/util.js
+++ logic/util.js
@@ -1,4 +1,27 @@
 function Util() {
+  const calculateTotalDistributedPassengers = (
+    distributedPassengers
+  ) => {
+    let totalDistributedPassengers = 0;
+    let value;
+    for (value in distributedPassengers) {
+       totalDistributedPassengers += distributedPassengers[value];
+    }
+    return totalDistributedPassengers;
+  };
 
+  const calculateTotalNumberOfPassengers = (passengersArray) => {
+    let totalNumberOfPassengers = 0;
+    for (let passengers of passengersArray) {
+      totalNumberOfPassengers += passengers;
+    }
+    return totalNumberOfPassengers;
+  };
+
+  return {
+    calculateTotalDistributedPassengers,
+    calculateTotalNumberOfPassengers,
+  };
 }
 
+module.exports = Util();
